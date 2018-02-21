b0624e0e12fae6c28f196f75f7d88cce685d1687
emi kojima <emi8emi@gmail.com>
2018-02-21 22:07:05 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,7 +107,7 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
     programmer_hash[:alan_kay] = {:alans_new_info => "Gui", :languages => ["Smalltalk", "LISP"]}
-     binding.pry
+    # binding.pry
 end
 
 def adding_to_dennis
