cec040da67cb301c08716edcf21bab3add3e7b42
emi kojima <emi8emi@gmail.com>
2018-02-21 21:55:00 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -108,7 +108,7 @@ def changing_alan
      alans_new_info = "GUI"
      programmer_hash[:alan_kay][:known_for] = [:alans_new_info]
      programmer_hash[:alan_kay][:alans_new_info] = "GUI"
-     
+     binding.pry
 end
 
 def adding_to_dennis
